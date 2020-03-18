326e7d37aa192c5d4976937bdf27eae5a5df0cee
vleup <kratz.philippe@gmail.com>
2020-03-18 15:29:49 +0100

    exo2-2

--- algo_php/partie2/exo1.php
+++ algo_php/partie2/exo1.php
@@ -1,3 +1,6 @@
 <?php
-    
+    function convertirMajRouge($phrase) {
+        return '<p style="color:red;">'.mb_strtoupper($phrase)."</p>";
+    }
+    echo convertirMajRouge("Mon texte en paramètre");
 ?>
---
+++ algo_php/partie2/exo2.php
@@ -0,0 +1,7 @@
+<?php
+    $capitales = array("France"->"Paris", "Allemagne"->"Berlin", "USA"->"Washington", "Italie"->"Rome");
+    
+    function afficherTableHTML($capitales) {
+        
+    }
+?>
