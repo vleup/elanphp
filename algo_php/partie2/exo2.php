d6c0b258ffc28d9a9f8da7c63596ec8309ee1b8d
vleup <kratz.philippe@gmail.com>
2020-03-18 16:41:41 +0100

    fin de journée

--- algo_php/partie2/exo2.php
+++ algo_php/partie2/exo2.php
@@ -1,7 +1,18 @@
 <?php
-    $capitales = array("France"->"Paris", "Allemagne"->"Berlin", "USA"->"Washington", "Italie"->"Rome");
+/*
+* wip
+*/
+    $capitales = array("France"=>"Paris", "Allemagne"=>"Berlin", "USA"=>"Washington", "Italie"=>"Rome");
     
     function afficherTableHTML($capitales) {
-        
+        foreach ($capitales as $key => $value) {
+            # code...
+        }
+        return ksort($table);
     }
+    echo '<table><tr><th style="strong">Pays</th><th style="strong">Capitale</th></tr>';
+    /*foreach ($capitales as $key => $value) {
+            echo "<tr><td>".mb_strtoupper($key)."</td><td>$value</td></tr>";
+        }*/
+    echo "</table>";
 ?>
